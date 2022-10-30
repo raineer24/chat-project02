5bdfb41c4dbc963a1023f18160496048d6dc0904
raineer <delaritaraineer81@gmail.com>
2022-10-30 17:36:09 +0800

    nestjs

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  getHello(): Object {
     return this.appService.getHello();
   }
 }
