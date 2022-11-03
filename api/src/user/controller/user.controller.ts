2f2b58f13596ed6d64f8a7bdc09a79c35b1035d9
raineer <delaritaraineer81@gmail.com>
2022-11-03 20:13:54 +0800

    auth module

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -7,8 +7,9 @@ import {
   Req,
   UseGuards,
 } from '@nestjs/common';
-import { Observable, of, switchMap } from 'rxjs';
+import { Observable, of, switchMap, from } from 'rxjs';
 import { CreateUserDto } from '../model/dto/create-user.dto';
+import { UserI } from '../model/user.interface';
 import { UserHelperService } from '../service/user-helper/user-helper.service';
 import { UserService } from '../service/user-service/user.service';
 
@@ -19,12 +20,36 @@ export class UserController {
     private userHelperService: UserHelperService,
   ) {}
 
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto): Observable<UserI> {
+  //   return this.userHelperService
+  //     .createUserDtoEntity(createUserDto)
+  //     .pipe(switchMap((user: UserI) => this.userService.create(user)));
+  // }
+
   @Post()
-  create(@Body() createUserDto: CreateUserDto): Observable<boolean> {
-    return this.userHelperService.createUserDtoEntity(createUserDto).pipe(
-      switchMap((user: UserI) => this.userService.create())
-    )
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserI> {
+    const userEntity: UserI =
+      this.userHelperService.createUserDtoToEntity(createUserDto);
+    return this.userService.create(userEntity);
   }
+
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto): Observable<UserI> {
+  //   return this.userHelperService
+  //     .createUserDtoEntity(createUserDto)
+  //     .pipe(switchMap((user: UserI) => this.userService.create(user)));
+  // }
+
+  // return from(this.userRepository.findOne({ email })).pipe(
+  //   map((user: UserI) => {
+  //     if (user) {
+  //       return true;
+  //     } else {
+  //       return false;
+  //     }
+  //   }),
+  // );
 
   // @Get()
   // findAll() {}
--- api/src/user/service/user-service/user.service.ts
+++ api/src/user/service/user-service/user.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, map, Observable } from 'rxjs';
+import { from, map, Observable, switchMap } from 'rxjs';
 import { UserEntity } from '../../model/user.entity';
 import { UserI } from 'src/user/model/user.interface';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from 'src/user/model/dto/create-user.dto';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
 
 @Injectable()
 export class UserService {
@@ -12,15 +16,99 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  private mailExists(email: string): Observable<boolean> {
-    return from(this.userRepository.findOne({ email })).pipe(
-      map((user: UserI) => {
-        if (user) {
-          return true;
-        } else {
-          return false;
-        }
-      }),
-    );
+  createUserDtoToEntity(createUserDto: CreateUserDto): UserI {
+    return {
+      email: createUserDto.email,
+      username: createUserDto.username,
+      password: createUserDto.password,
+    };
+  }
+
+  async create(newUser: UserI): Promise<UserI> {
+    try {
+      const exists: boolean = await this.mailExists(newUser.email);
+      if (!exists) {
+        const passwordHash: string = await this.hashPassword(newUser.password);
+        newUser.password = passwordHash;
+        const user = await this.userRepository.save(
+          this.userRepository.create(newUser),
+        );
+        return this.findOne(user.id);
+      } else {
+        throw new HttpException('Email is already in use', HttpStatus.CONFLICT);
+      }
+    } catch {
+      throw new HttpException('Email is already in use', HttpStatus.CONFLICT);
+    }
+  }
+
+  // create(newUser: UserI): Observable<UserI> {
+  //   return this.mailExists(newUser.email).pipe(
+  //     switchMap((exists: boolean) => {
+  //       if (!exists) {
+  //         return this.hashPassword(newUser.password).pipe(
+  //           switchMap((passwordHash: string) => {
+  //             //overwrite the user password with the hash, to store thash in the database
+  //             newUser.password = passwordHash;
+  //             return from(this.userRepository.save(newUser)).pipe(
+  //               switchMap((user: UserI) => this.findOne(user.id)),
+  //             );
+  //           }),
+  //         );
+  //       }
+  //     }),
+  //   );
+  // }
+
+  // create(newUser: UserI): Observable<UserI> {
+  //   return this.mailExists(newUser.email).pipe(
+  //     switchMap((exists: boolean) => {
+  //       if (exists === true) {
+  //         return this.hashPassword(newUser.password).pipe(
+  //           switchMap((passwordHash: string) => {
+  //             //overwite the user password with the hash, to store the hash in the database
+  //             newUser.password = passwordHash;
+  //             return from(this.userRepository.save(newUser)).pipe(
+  //               switchMap((user: UserI) => this.findOne(user.id)),
+  //             );
+  //           }),
+  //         );
+  //       } else {
+  //         throw new HttpException(
+  //           'Email is already in use',
+  //           HttpStatus.CONFLICT,
+  //         );
+  //       }
+  //     }),
+  //   );
+  // }
+
+  private hashPassword(password: string): Observable<string> {
+    return from<string>(bcrypt.hash(password, 12));
+  }
+
+  private findOne(id: number): Observable<UserI> {
+    return from(this.userRepository.findOne(id));
+  }
+
+  // private mailExists(email: string): Observable<boolean> {
+  //   return from(this.userRepository.findOne({ email })).pipe(
+  //     map((user: UserI) => {
+  //       if (user) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     }),
+  //   );
+  // }
+
+  private async mailExists(email: string): Promise<boolean> {
+    const user = await this.userRepository.findOne({ email });
+    if (user) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
