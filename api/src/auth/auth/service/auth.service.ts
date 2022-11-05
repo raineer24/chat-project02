0038e994ac18ab999346fd9d44bf922d051fa5e0
raineer <delaritaraineer81@gmail.com>
2022-11-05 15:28:47 +0800

    user service auth module

--- api/src/auth/auth/service/auth.service.ts
+++ api/src/auth/auth/service/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcrypt');
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+}
