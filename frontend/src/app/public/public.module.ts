0670b948cda378836bd58bc08667ea0db074c42b
raineer <delaritaraineer81@gmail.com>
2022-11-09 02:53:05 +0800

    private public routing module

--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { PublicRoutingModule } from './public-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PublicRoutingModule
   ]
 })
 export class PublicModule { }
