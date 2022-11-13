a2e4db96345a34a64c80edce329daae70b4a911c
raineer <delaritaraineer81@gmail.com>
2022-11-14 00:30:50 +0800

    verifyJwt

---
+++ api/src/middleware/auth.middleware.ts
@@ -0,0 +1,25 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { AuthService } from 'src/auth/service/auth.service';
+import { UserI } from 'src/user/model/user.interface';
+import { UserService } from 'src/user/service/user-service/user.service';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    try {
+      const tokenArray: string[] = req.headers['authorization'].split(' ');
+    } catch {
+      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
