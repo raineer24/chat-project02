1b303017acb49dbed733dcdff66e4b48d1b6be47
raineer <delaritaraineer81@gmail.com>
2022-11-17 22:11:35 +0800

    room entity

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { ChatGateway } from './gateway/chat.gateway';
 import { RoomService } from './service/room-service/room/room.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomEntity } from './model/room/room.entity';
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, TypeOrmModule.forFeature([RoomEntity])],
   providers: [ChatGateway, RoomService],
 })
 export class ChatModule {}
--- api/src/chat/service/room-service/room/room.service.ts
+++ api/src/chat/service/room-service/room/room.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
+import { RoomEntity } from 'src/chat/model/room/room.entity';
+import { RoomI } from 'src/chat/model/room/room.interface';
+import { UserI } from 'src/user/model/user.interface';
+import { Repository } from 'typeorm';
 @Injectable()
 export class RoomService {}
