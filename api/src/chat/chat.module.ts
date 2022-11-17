1574ccfe0bc274fc4719efc86c2dd2dcd73ad734
raineer <delaritaraineer81@gmail.com>
2022-11-17 16:45:50 +0800

    chat service

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
 import { ChatGateway } from './gateway/chat.gateway';
+import { RoomService } from './service/room-service/room/room.service';
 
 @Module({
   imports: [AuthModule, UserModule],
-  providers: [ChatGateway],
+  providers: [ChatGateway, RoomService],
 })
 export class ChatModule {}
