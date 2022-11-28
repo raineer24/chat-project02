b2c49702f838b1450102a0c7b266f72b826fa493
raineer <delaritaraineer81@gmail.com>
2022-11-29 02:34:20 +0800

    connectuser service

--- api/src/chat/service/connected-user/connected-user.service.ts
+++ api/src/chat/service/connected-user/connected-user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ConnectedUserEntity } from 'src/chat/model/connected-user/connected-user.entity';
+import { ConnectedUserI } from 'src/chat/model/connected-user/connected-user.interface';
+import { UserI } from 'src/user/model/user.interface';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ConnectedUserService {}
+export class ConnectedUserService {
+  constructor(
+    @InjectRepository(ConnectedUserEntity)
+    private readonly connectedUserRepository: Repository<ConnectedUserEntity>,
+  ) {}
+
+  async create() {
+    return;
+  }
+  async findByUser() {
+    return;
+  }
+  async deleteBySocketId() {
+    return;
+  }
+  async deleteAll() {
+    return;
+  }
+}
