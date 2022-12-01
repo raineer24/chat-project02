2cd070218c4951d2ed9d2a2fc25efa069857dd6e
raineer <delaritaraineer81@gmail.com>
2022-12-01 21:54:51 +0800

    messageEntity

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -7,11 +7,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoomEntity } from './model/room/room.entity';
 import { ConnectedUserService } from './service/connected-user/connected-user.service';
 import { ConnectedUserEntity } from './model/connected-user/connected-user.entity';
+import { MessageEntity } from './model/message/message.entity';
 @Module({
   imports: [
     AuthModule,
     UserModule,
-    TypeOrmModule.forFeature([RoomEntity, ConnectedUserEntity]),
+    TypeOrmModule.forFeature([MessageEntity, RoomEntity, ConnectedUserEntity]),
   ],
   providers: [ChatGateway, RoomService, ConnectedUserService],
 })
