c297a7bdd0cc41371985a2b97026289d43a74178
raineer <delaritaraineer81@gmail.com>
2022-12-06 03:00:57 +0800

    message interface ts

---
+++ frontend/src/app/model/message.interface.ts
@@ -0,0 +1,12 @@
+import { Meta } from "@angular/platform-browser";
+import { RoomI } from "./room.interface";
+import { UserI } from "./user.interface";
+
+export interface MessageI {
+    id?: number;
+    text: string;
+    user?: UserI;
+    room: RoomI;
+    created_at?: Date;
+    updated_at?: Date;
+}
