12a53b8b6f51d2024282ecc5cd832aea4cb8ed1a
raineer <delaritaraineer81@gmail.com>
2022-12-07 00:01:03 +0800

    chat message ;

--- frontend/src/app/private/components/chat-room/chat-room.component.ts
+++ frontend/src/app/private/components/chat-room/chat-room.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { combineLatest, Observable } from 'rxjs';
 import { MessagePaginateI } from 'src/app/model/message.interface';
 import { RoomI } from 'src/app/model/room.interface';
@@ -13,7 +22,31 @@ export class ChatRoomComponent implements OnChanges, OnDestroy, AfterViewInit {
   @Input() chatRoom: RoomI;
 
   messages$: Observable<MessagePaginateI> = this.chatService.getMessages();
+
+  chatMessage: FormControl = new FormControl(null, [Validators.required]);
   constructor(private chatService: ChatService) {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.chatRoom) {
+      this.chatService.joinRoom(this.chatRoom);
+    }
+  }
+
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy() {
+    this.chatService.leaveRoom(this.chatRoom);
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage({
+      text: this.chatMessage.value,
+      room: this.chatRoom,
+    });
+    this.chatMessage.reset();
+  }
 }
