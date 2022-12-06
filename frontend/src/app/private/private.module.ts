73024ac403b56ea41a6501f71ee2264b13099dbf
raineer <delaritaraineer81@gmail.com>
2022-12-06 22:52:18 +0800

    generate components

--- frontend/src/app/private/private.module.ts
+++ frontend/src/app/private/private.module.ts
@@ -15,8 +15,9 @@ import {MatChipsModule} from '@angular/material/chips';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatIconModule} from '@angular/material/icon';
 import { SelectUsersComponent } from './components/select-users/select-users.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
 @NgModule({
-  declarations: [DashboardComponent, CreateRoomComponent, SelectUsersComponent],
+  declarations: [DashboardComponent, CreateRoomComponent, SelectUsersComponent, ChatRoomComponent],
   imports: [
     CommonModule,
     PrivateRoutingModule,
