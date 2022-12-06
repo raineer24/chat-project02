0aa1feb08f1b42297a6bf88c2ab0315bd852b7ea
raineer <delaritaraineer81@gmail.com>
2022-12-06 23:32:22 +0800

    chatroom component ;

--- frontend/src/app/private/components/chat-room/chat-room.component.ts
+++ frontend/src/app/private/components/chat-room/chat-room.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { combineLatest, Observable } from 'rxjs';
+import { MessagePaginateI } from 'src/app/model/message.interface';
+import { RoomI } from 'src/app/model/room.interface';
+import { ChatService } from '../../services/chat-service/chat.service';
 
 @Component({
   selector: 'app-chat-room',
   templateUrl: './chat-room.component.html',
-  styleUrls: ['./chat-room.component.scss']
+  styleUrls: ['./chat-room.component.scss'],
 })
-export class ChatRoomComponent implements OnInit {
+export class ChatRoomComponent implements OnChanges, OnDestroy, AfterViewInit {
+  @Input() chatRoom: RoomI;
 
-  constructor() { }
+  messages$: Observable<MessagePaginateI> = this.chatService.getMessages();
+  constructor(private chatService: ChatService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
